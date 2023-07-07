d28fafa498ebad850c7fa9712fdb526bf2864d3e
said molina <68343481+said-molina@users.noreply.github.com>
2023-07-06 20:21:29 -0600

    empty-project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>Rick and Morty</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
